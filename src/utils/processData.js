19d82e38b768b6b8dfbd2acf8c6789d84136b12b
ben40d9 <benpaley6@gmail.com>
2023-07-03 20:33:16 -0700

    Commit message: "Integrate LangChain's OpenAI embeddings in processData function"
    Description:
    "Integrated LangChain's OpenAI embeddings into the processData function. The function now creates embeddings for the Comment and Reply3 fields of each item in the data, and uses these embeddings as the vector for Pinecone. Also added error handling for cases where creating embeddings fails or the fields are empty."

--- src/utils/processData.js
+++ src/utils/processData.js
@@ -1,9 +1,48 @@
-//process data to be used in the chart
-export function processData(data) {
-  return data.map((item, index) => {
-    return {
-      id: index.toString(),
-      vector: [item.Comment, item.Reply3],
-    };
-  });
+// export function processData(data) {
+//   return data.map((item, index) => {
+//     console.log(
+//       `Processing item ${index}: Comment = ${item.Comment}, Reply3 = ${item.Reply3}`
+//     );
+//     return {
+//       id: index.toString(),
+//       vector: [item.Comment, item.Reply3],
+//     };
+//   });
+// }
+
+import { LangChain, Embeddings } from "langchain";
+
+import "dotenv/config";
+
+export async function processData(data) {
+  // Initialize LangChainJS with your OpenAI API key
+  const langChain = new LangChain({ apiKey: process.env.OPENAI_API_KEY });
+
+  // Create an instance of the Embeddings class
+  const embeddings = new Embeddings(langChain, "model-name");
+
+  // Process the data
+  const processedData = [];
+  for (const item of data) {
+    let commentEmbedding = [];
+    let reply3Embedding = [];
+
+    try {
+      // Create embeddings for the Comment and Reply3 fields
+      commentEmbedding = item.Comment
+        ? await embeddings.create(item.Comment)
+        : [];
+      reply3Embedding = item.Reply3 ? await embeddings.create(item.Reply3) : [];
+    } catch (error) {
+      console.error("Error creating embeddings:", error);
+    }
+
+    // Add the processed item to the processedData array
+    processedData.push({
+      id: item.id.toString(),
+      vector: [...commentEmbedding, ...reply3Embedding],
+    });
+  }
+
+  return processedData;
 }
