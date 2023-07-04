e35e26ee30659f10b1c856dd9cd7881c237fa608
ben40d9 <benpaley6@gmail.com>
2023-07-03 20:32:46 -0700

    Commit message: "Fix vector structure in ingestData function"
    Description:
    "Changed the structure of the vectors being ingested into Pinecone. Previously, the vectors field was a list containing a single item, which was another list of vectors. Now, the vectors field is a list of vectors. This fixes an error where Pinecone was expecting a vector dimension of 768 but was receiving a dimension of 0."

--- src/utils/ingestData.js
+++ src/utils/ingestData.js
@@ -1,28 +1,30 @@
-// import { PineconeClient } from "@pinecone-database/pinecone";
-
-// // const PINECONE_API_KEY = "";
-
-// // const indexName = "tiktok-comments";
-
-// export async function ingestData(data, PINECONE_API_KEY, indexName) {
-//   const client = new PineconeClient();
-
-//   await client.init(PINECONE_API_KEY);
-//   //   client.init(PINECONE_API_KEY);
-
-//   const index = client.Index("tiktok-comments");
-
-//   // Upsert the data into the index
-//   const upsertResponse = await index.upsert(
-//     indexName,
-//     data.map((item) => ({ id: item.id, vector: item.vector }))
-//   );
-//   console.log("Data successfully ingested into Pinecone:", upsertResponse);
-// }
-
 import { PineconeClient } from "@pinecone-database/pinecone";
+
 import "dotenv/config";
 
+// export async function ingestData(data) {
+//   try {
+//     const client = new PineconeClient();
+
+//     await client.init({
+//       environment: "northamerica-northeast1-gcp" ?? "",
+//       apiKey: process.env.PINECONE_API_KEY ?? "",
+//     });
+
+//     const index = client.Index("tiktok-comments");
+
+//     // Upsert the data into the index
+//     const upsertResponse = await index.upsert({
+//       upsertRequest: {
+//         vectors: data.map((item) => ({ id: item.id, values: item.vector })),
+//         namespace: "tiktok-namespace",
+//       },
+//     });
+//     console.log("Data successfully ingested into Pinecone:", upsertResponse);
+//   } catch (error) {
+//     console.error("Error ingesting data into Pinecone:", error);
+//   }
+// }
 export async function ingestData(data) {
   try {
     const client = new PineconeClient();
@@ -34,10 +36,16 @@ export async function ingestData(data) {
 
     const index = client.Index("tiktok-comments");
 
+    // Log the data that is being ingested
+    console.log(
+      "Ingesting data:",
+      data.map((item) => ({ id: item.id, values: item.vector }))
+    );
+
     // Upsert the data into the index
     const upsertResponse = await index.upsert({
       upsertRequest: {
-        vectors: [data.map((item) => ({ id: item.id, values: item.vector }))],
+        vectors: data.map((item) => ({ id: item.id, values: item.vector })),
         namespace: "tiktok-namespace",
       },
     });
