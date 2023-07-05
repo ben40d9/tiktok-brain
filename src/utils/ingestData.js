84bfd4725d06de9d8078190479a9fd0798cea84a
ben40d9 <benpaley6@gmail.com>
2023-07-05 16:28:01 -0700

    values only peing processed if they have comment and a reply, also saving to create new branch

--- src/utils/ingestData.js
+++ src/utils/ingestData.js
@@ -2,29 +2,6 @@ import { PineconeClient } from "@pinecone-database/pinecone";
 
 import "dotenv/config";
 
-// export async function ingestData(data) {
-//   try {
-//     const client = new PineconeClient();
-
-//     await client.init({
-//       environment: "northamerica-northeast1-gcp" ?? "",
-//       apiKey: process.env.PINECONE_API_KEY ?? "",
-//     });
-
-//     const index = client.Index("tiktok-comments");
-
-//     // Upsert the data into the index
-//     const upsertResponse = await index.upsert({
-//       upsertRequest: {
-//         vectors: data.map((item) => ({ id: item.id, values: item.vector })),
-//         namespace: "tiktok-namespace",
-//       },
-//     });
-//     console.log("Data successfully ingested into Pinecone:", upsertResponse);
-//   } catch (error) {
-//     console.error("Error ingesting data into Pinecone:", error);
-//   }
-// }
 export async function ingestData(data) {
   try {
     const client = new PineconeClient();
@@ -45,8 +22,10 @@ export async function ingestData(data) {
     // Upsert the data into the index
     const upsertResponse = await index.upsert({
       upsertRequest: {
-        vectors: data.map((item) => ({ id: item.id, values: item.vector })),
-        namespace: "tiktok-comments",
+        vectors: data.map((item) => ({
+          id: item.id ?? "",
+          values: item.vector ?? "",
+        })),
       },
     });
     console.log("Data successfully ingested into Pinecone:", upsertResponse);
@@ -54,3 +33,24 @@ export async function ingestData(data) {
     console.error("Error ingesting data into Pinecone:", error);
   }
 }
+
+// export async function ingestData(data) {
+//   try {
+//     const embedChain = await EmbedChain(
+//       `${process.env.PINECONE_API_KEY}`,
+//       "tiktok-comments"
+//     );
+
+//     // Log the data that is being ingested
+//     console.log(
+//       "Ingesting data:",
+//       data.map((item) => ({ id: item.id, values: item.vector }))
+//     );
+
+//     // Upsert the data into the index
+//     const upsertResponse = await embedChain.upsert(data);
+//     console.log("Data successfully ingested into Pinecone:", upsertResponse);
+//   } catch (error) {
+//     console.error("Error ingesting data into Pinecone:", error);
+//   }
+// }
--- src/utils/processData.js
+++ src/utils/processData.js
@@ -66,15 +66,21 @@
 // }
 
 export function processData(data) {
-  return data.map((item, index) => {
-    console.log(
-      `Processing item ${index}: Comment = ${item.Comment}, Reply3 = ${item.Reply3}`
-    );
-    return {
-      id: index.toString(),
-      vector: [item.Comment, item.Reply3],
-    };
-  });
+  return (
+    data
+
+      // Remove any rows that don't have a value for the Comment or Reply3 fields
+      .filter((item) => item.Comment.trim() !== "" && item.Reply3.trim() !== "")
+      .map((item, index) => {
+        console.log(
+          `Processing item ${index}: Comment = ${item.Comment}, Reply3 = ${item.Reply3}`
+        );
+        return {
+          id: index.toString(),
+          vector: [item.Comment, item.Reply3],
+        };
+      })
+  );
 }
 
 // import { OpenAI } from "langchain";
